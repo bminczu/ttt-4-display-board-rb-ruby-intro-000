2d5b3b9c14b8c36d3e4df97fbcf04ed7e2062c10
adventurous-format-6947 <bminczu@gmail.com>
2020-09-22 17:07:20 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,1 @@
-board = [" ", " ", " ", " ", " ", "X", " ", " ", " "]
-def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-
-end
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
