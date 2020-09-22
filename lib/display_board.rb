55771176a0ccdff0b61938cd05df8d909e2078d0
adventurous-format-6947 <bminczu@gmail.com>
2020-09-22 17:26:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@ board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "#{board[4]}"
   puts "-----------"
   puts "   |   |   "
   puts "#{board[4]}"
